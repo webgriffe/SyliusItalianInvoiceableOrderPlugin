6f52cd893b40af71b00fbd8e7213db2f15d378d2
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-12-16 16:56:48 +0100

    Align to Sylius PluginSkeleton 1.12

--- src/Validator/Constraints/ItalianVatNumberValidator.php
+++ src/Validator/Constraints/ItalianVatNumberValidator.php
@@ -11,7 +11,7 @@ use Symfony\Component\Validator\Exception\UnexpectedValueException;
 
 final class ItalianVatNumberValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint): void
+    public function validate(mixed $value, Constraint $constraint): void
     {
         if (!$constraint instanceof ItalianVatNumber) {
             throw new UnexpectedTypeException($constraint, ItalianVatNumber::class);
