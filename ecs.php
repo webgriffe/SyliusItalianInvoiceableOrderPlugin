c9126ae7e6a7eb90e0e0344938adfff9f1f923ff
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-10-15 16:34:11 +0200

    Fix CI

--- ecs.php
+++ ecs.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
 
-    $ecsConfig->parameters()->set(Option::SKIP, [
+    $ecsConfig->skip([
         VisibilityRequiredFixer::class => ['*Spec.php'],
     ]);
 };
