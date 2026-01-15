8e9ee011c66386f7690e7b2328c59a160f5aa6fa
Luca Gallinari <luke.gallinari@gmail.com>
2026-01-15 11:33:33 +0100

    Add missing Vies Bundle

--- tests/TestApplication/config/bundles.php
+++ tests/TestApplication/config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    Sandwich\ViesBundle\SandwichViesBundle::class => ['all' => true],
     Webgriffe\SyliusItalianInvoiceableOrderPlugin\WebgriffeSyliusItalianInvoiceableOrderPlugin::class => ['all' => true],
 ];
